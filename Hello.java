b428a713a38f9cdc4f7e58517550b744aaa81150
JinHyeonKim <imrnortality@naver.com>
2015-12-29 09:26:57 +0900

    GitHub for git pull

--- Hello.java
+++ Hello.java
@@ -4,12 +4,11 @@
 public class Hello {
 	public static void main(String[] args){
 		System.out.println("Hello Git!");
-
 		System.out.println("What is Git?!");
 		System.out.println("That's DVCS");
-
 		System.out.println("What is Git?");
 		System.out.println("2002 Bitkeeper");
+		System.out.println("That's DVCS!");
 		}
 	}
 
